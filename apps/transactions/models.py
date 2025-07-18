f60e1bf0fd2960b067329ae4cf98951b4fb2eff0
Viktor Rura <ruravictor@gmail.com>
2025-07-18 13:28:32 +0100

    Add Transaction model

--- apps/transactions/models.py
+++ apps/transactions/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from apps.core.models import TimestampedModel
 
-# Create your models here.
+
+class Transaction(TimestampedModel):
+    transaction_code = models.CharField(max_length=20, unique=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    type = models.CharField(
+        max_length=10, choices=[("deposit", "Deposit"), ("expense", "Expense")]
+    )
+    transaction_date = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.transaction_code} - {self.type} - ${self.amount}"
