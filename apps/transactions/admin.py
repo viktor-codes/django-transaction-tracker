feba724d2f4fcf0c9be7bce83e02437a5e5bf3cf
Viktor Rura <ruravictor@gmail.com>
2025-07-19 13:30:33 +0100

    Register transaction model in django admin

--- apps/transactions/admin.py
+++ apps/transactions/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Transaction
 
-# Register your models here.
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = [
+        "transaction_code",
+        "type",
+        "amount",
+        "transaction_date",
+        "created_at",
+    ]
+    list_filter = ["type", "created_at"]
+    search_fields = ["transaction_code"]
+    readonly_fields = ["created_at", "updated_at"]
+    ordering = ["-transaction_date"]
